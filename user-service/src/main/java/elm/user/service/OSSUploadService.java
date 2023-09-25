637cdcc97cadf3689e99c5b94de7d7cde7471b23
zzw <3279527604@qq.com>
2023-09-25 15:08:31 +0800

    七牛云OSS

---
+++ user-service/src/main/java/elm/user/service/OSSUploadService.java
@@ -0,0 +1,14 @@
+package elm.user.service;
+
+import elm.common.domain.ResponseResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface OSSUploadService {
+
+    /**
+     * 七牛云对象存储上传头像
+     * @param file
+     * @return
+     */
+    ResponseResult uploadImg(MultipartFile file);
+}
