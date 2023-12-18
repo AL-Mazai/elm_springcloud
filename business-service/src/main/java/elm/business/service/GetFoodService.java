1d7c7f23db1dcd33e51924e976787b33672a69e9
zzw <3279527604@qq.com>
2023-12-18 17:31:35 +0800

    引入feign实现服务之间的通信

---
+++ business-service/src/main/java/elm/business/service/GetFoodService.java
@@ -0,0 +1,13 @@
+package elm.business.service;
+
+import elm.common.domain.ResponseResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("food-service")
+public interface GetFoodService {
+
+    @RequestMapping("/food/foodList")
+    ResponseResult getFoodOfBusiness(@RequestParam("businessId") Integer businessId);
+}
